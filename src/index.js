519115921dd62c60788376d935afec924e9008d8
Masegolex <160056223+Masegolex@users.noreply.github.com>
2024-04-21 17:40:22 +0200

    Added search functionality using API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature"); // Use # for ID
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city"); // Use # for ID
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature); // Corrected spelling of temperature
+}
+
+function searchCity(city) {
+  let apiKey = "5of9b61e3227f64b3eaa050fcft44f04";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
-  event.preventDefault(); // Prevent the default form submission behavior
-  let searchInput = document.querySelector("#search-form-input"); // Get the input element
-  let cityElement = document.querySelector("#city"); // Get the city element
-  cityElement.textContent = searchInput.value;
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Cape Town");
